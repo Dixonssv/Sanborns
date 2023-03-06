d2695bf9854d91f0be6d24f40cef9156871f2d7a
Dixonssv <santiago_cuarto@hotmail.com>
2023-03-06 14:00:36 -0600

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HttpClientTestComponent } from './http-client-test/http-client-test.component';
+import { Compo1Component } from './components/compo1/compo1.component';
+import { Mod2Module } from './mod2.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Compo1Component,
+    HttpClientTestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    Mod2Module
   ],
   providers: [],
   bootstrap: [AppComponent]
