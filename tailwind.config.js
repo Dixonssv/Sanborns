6597a4a86364c753285990c8633e4b20041305ce
Dixonssv <santiago_cuarto@hotmail.com>
2023-03-13 15:32:14 -0600

    estilo y correccion de tailwind-config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/app.component.{html,ts}",
-    "./src/app/components/buscador/buscador.component.{html, ts}",
-    "./src/app/components/detalles/detalles.component.{html, ts}",
-    "./src/app/components/listado-empleados/listado-empleados.component.{html, ts}",
-    "./src/app/components/header/header.component.{html, ts}",
+    "./src/index.html",
+    "./src/app/app.component.html",
+    "./src/app/components/buscador/buscador.component.html",
+    "./src/app/components/detalles/detalles.component.html",
+    "./src/app/components/listado-empleados/listado-empleados.component.html",
+    "./src/app/components/header/header.component.html"
   ],
   theme: {
     extend: {},
