dffc1e62168c0e7ab400e317c8746b8c9bc6746f
Dixonssv <santiago_cuarto@hotmail.com>
2023-03-15 11:59:10 -0600

    'texto_resaltado'

--- src/app/components/listado-empleados/listado-empleados.component.ts
+++ src/app/components/listado-empleados/listado-empleados.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { BuscadorEmpleadosService } from 'src/app/services/buscador-empleados.service';
 
 @Component({
   selector: 'app-listado-empleados',
   templateUrl: './listado-empleados.component.html',
-  styleUrls: ['./listado-empleados.component.css']
+  styleUrls: ['./listado-empleados.component.css'],
+  encapsulation: ViewEncapsulation.None, // Aplicar estilos a innerHTML
 })
 export class ListadoEmpleadosComponent {
 
