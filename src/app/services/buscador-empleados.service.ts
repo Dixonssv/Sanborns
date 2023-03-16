cb7dd49a1ea5193b570dda1bb86f1c0aa6732777
Dixonssv <santiago_cuarto@hotmail.com>
2023-03-16 14:56:11 -0600

    Optimizacion busqueda - Busqueda local en set

--- src/app/services/buscador-empleados.service.ts
+++ src/app/services/buscador-empleados.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { NumberFormatStyle } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -9,28 +10,72 @@ export class BuscadorEmpleadosService {
 
   urlApi = 'https://administration.gsanborns.com.mx/api/empleados/buscar';
 
-  result : any;
-
+  result : any = [];
+  
   constructor(private http : HttpClient) { }
 
-  async getEmpleado(empleado : string) {
+  getEmpleado(cadena : string) {
+    if(cadena == "") {
+      this.result = []
+    } else if(!this.buscarEnSet(cadena)) {
+      this.buscarEnApi(cadena);
+    }
+  }
+
+  buscarEnApi(cadena:string) {
+    console.log("--> Llamada a la API -->");
+
     const options = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
         'Authorization': `Basic ${btoa('santiago:2LchKVi2$EX*w*&8TlE64FpaSX0uCp4MqoEUC%yr0o*yf6FGEX')}`
       }),
-      params: new HttpParams().set("query", empleado)
-    }
-
-    if(empleado != "") {
-      this.http.get(this.urlApi, options).subscribe((data:any) => {
-        this.result = data.body;
-      });
-    } else {
-      this.result = [];
+      params: new HttpParams().set("query", cadena)
     }
     
-    /*
+    this.http.get(this.urlApi, options).subscribe((data:any) => {
+      this.result = data.body;
+    });
+  }
+
+  buscarEnSet(cadena:string) {
+    cadena = cadena.toUpperCase();
+
+    let newResultSet:any = [];
+
+    // Busca coincidencias en el set actual
+    this.result.forEach((empleado:any, index:number) => {
+      if(empleado.nombre.includes(cadena)) {
+        empleado._formatted.nombre = this.reformat(empleado, cadena);
+        newResultSet.push(empleado);
+      } else {
+        console.log("Se elimina " + empleado.nombre);
+      }
+
+      this.result = newResultSet;
+    });
+
+    if(this.result.length == 0) {
+      return false;
+    }
+
+    return true;
+  }
+
+  reformat(empleado:any, cadena:string):string {
+    let nombre = empleado.nombre;
+    return nombre.replace(cadena, "<em>" + cadena + "</em>");
+  }
+
+  getDetallesEmpleado(index:number) {
+    return this.result[index];
+  }
+}
+
+
+
+// -----------------------------------------------------------------------
+/*
     {
       "claveCompania": 1,
       "compania": "SANBORN HERMANOS",
@@ -61,9 +106,3 @@ export class BuscadorEmpleadosService {
       }
     }
     */
-  }
-
-  getDetallesEmpleado(index:number) {
-    return this.result[index];
-  }
-}
